2c56db602b7e358693896ba2cd8ffde4cba300ad
Volker Hartmann <volker.hartmann@kit.edu>
2019-11-18 17:44:45 +0100

    Add dependency for Tool 1.5 and change dependency for grid-util.

--- buildMDM-MasiProcessors.sh
+++ buildMDM-MasiProcessors.sh
@@ -19,6 +19,12 @@ rm -rf *
 wget https://github.com/kit-data-manager/base/archive/KITDM_$KIT_DM_VERSION.zip
 unzip KITDM_$KIT_DM_VERSION.zip
 
+# Copy dependency of Tools 1.5 to lib 1.6
+cp -r $MASI_PROCESSORS_BASE/lib/org/fzk/ipe/Tools/1.5  base-KITDM_$KIT_DM_VERSION/libs/org/fzk/ipe/Tools/
+
+# Change dependency of grid-util from Tools 1.5 to Tools 1.6
+cp $MASI_PROCESSORS_BASE/lib/org/fzk/ipe/grid-util/2.1/grid-util-2.1.pom  base-KITDM_$KIT_DM_VERSION/libs/org/fzk/ipe/grid-util/2.1/grid-util-2.1.pom
+
 # Build libraries needed for MetaStore
 cd base-KITDM_$KIT_DM_VERSION
 mvn $MAVEN_OPTS clean install -P Release,generate-doc,test
